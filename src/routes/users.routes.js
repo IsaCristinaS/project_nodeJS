6e885b3f134d22882aaa43208e76bf53ade2622c
Isa <bela.tina@hotmail.com>
2024-07-29 15:56:31 -0300

    first commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+const userController = new UsersController();
+
+usersRoutes.post("/", userController.create);
+usersRoutes.put("/:id", userController.update);
+
+module.exports = usersRoutes;
+//linha 11: forma de expor o usersRoutes para que qualquer arquivo possa utilizar
